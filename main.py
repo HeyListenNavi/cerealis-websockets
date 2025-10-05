458cb886647a637163353fee1f2bde2fba474afe
Veronica Guzman Ochoa <heylistennavi@proton.me>
2025-10-05 01:19:45 -0700

    Update websocket server code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class ConnectionManager:
         self.clients_rgb: list[WebSocket] = []
         self.clients_depth: list[WebSocket] = []
         self.control_clients: list[WebSocket] = []
+        self.clients_info: list[WebSocket] = []
 
     async def connect_drone_rgb(self, websocket: WebSocket):
         await websocket.accept()
@@ -21,6 +22,10 @@ class ConnectionManager:
     async def connect_control_client(self, websocket: WebSocket):
         await websocket.accept()
         self.control_clients.append(websocket)
+    
+    async def connect_drone_info(self, websocket: WebSocket):
+        await websocket.accept()
+        self.clients_info.append(websocket)
 
     def disconnect_client_rgb(self, websocket: WebSocket):
         self.clients_rgb.remove(websocket)
@@ -30,6 +35,9 @@ class ConnectionManager:
 
     def disconnect_control_client(self, websocket: WebSocket):
         self.control_clients.remove(websocket)
+
+    def disconnect_client_info(self, websocket: WebSocket):
+        self.clients_info.remove(websocket)
 
     async def broadcast_rgb(self, data: bytes):
         for ws in self.clients_rgb:
@@ -52,6 +60,13 @@ class ConnectionManager:
             except:
                 self.control_clients.remove(ws)
 
+    async def broadcast_info(self, data: str):
+        for ws in self.clients_info:
+            try:
+                await ws.send_text(data)
+            except:
+                self.clients_info.remove(ws)
+
 manager = ConnectionManager()
 
 @app.websocket("/ws/rgb_feed")
@@ -63,7 +78,7 @@ async def rgb_feed(websocket: WebSocket):
             data = await websocket.receive_bytes()
             await manager.broadcast_rgb(data)
     except WebSocketDisconnect:
-        manager.disconnect_client_rgb()
+        manager.disconnect_client_rgb(websocket) 
         print("RGB feed disconnected")
 
 @app.websocket("/ws/depth_feed")
@@ -75,7 +90,7 @@ async def depth_feed(websocket: WebSocket):
             data = await websocket.receive_bytes()
             await manager.broadcast_depth(data)
     except WebSocketDisconnect:
-        manager.disconnect_client_depth()
+        manager.disconnect_client_depth(websocket)
         print("Depth feed disconnected")
 
 @app.websocket("/ws/control")
@@ -90,5 +105,24 @@ async def control(websocket: WebSocket):
         manager.disconnect_control_client(websocket)
         print("Control client disconnected")
 
+@app.websocket("/ws/drone_info")
+async def drone_info(websocket: WebSocket):
+    await manager.connect_drone_info(websocket)
+    print("Drone info client connected")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast_info(data)
+    except WebSocketDisconnect:
+        manager.disconnect_client_info(websocket)
+        print("Drone info client disconnected")
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        ws_ping_interval=None,
+        ws_ping_timeout=None  
+    )
